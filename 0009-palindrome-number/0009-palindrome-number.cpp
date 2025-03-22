509b9a76ed63d80ac3c3c7c38d416b3c4aa6841a
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-22 22:07:48 +0530

    Time: 6 ms (19.03%), Space: 8.7 MB (42.28%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
-        vector<int> myvec; vector<int> revvec;
-        int num = x;
-        while(num > 0){
-            myvec.insert(myvec.begin(), num%10);
-            revvec.push_back(num%10);
-            num = num/10;
+        // if (x < 0) return false;
+        // vector<int> myvec; vector<int> revvec;
+        // int num = x;
+        // while(num > 0){
+        //     myvec.insert(myvec.begin(), num%10);
+        //     revvec.push_back(num%10);
+        //     num = num/10;
+        // }
+        // if (myvec == revvec) return true;
+        // else return false;
+
+        //otherlogic!!
+        
+        if (x < 0 || (x%10 == 0 && x != 0)) return false;
+        int revhalf = 0;
+        while (x > revhalf){
+            revhalf = revhalf * 10 + x%10;
+            x /= 10;
         }
-        if (myvec == revvec) return true;
-        else return false;
+        return x == revhalf || x == revhalf/10;
     };
 };
