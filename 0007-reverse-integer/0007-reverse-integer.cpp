a1872a6aaf1c87984f36d5095e325c51b8496109
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-22 23:04:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.65%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int sign = 1;
+        if (x >= INT_MAX || x <= INT_MIN) return 0;
+        if (x < 0) {
+            x = -x;
+            sign = 0;
+        }
+        long int y = 0;
+        while(x > 0) {
+            y = y*10 + x%10;
+            x /= 10;
+        }
+        if (y >= INT_MAX || y <= INT_MIN) return 0;
+        if (sign) return y;
+        else return -y;
+    }
+};
