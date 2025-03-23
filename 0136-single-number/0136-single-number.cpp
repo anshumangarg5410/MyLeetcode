8be9fd02cff3a1f7ba458358e07ec81467a6e0d3
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-23 23:16:52 +0530

    Time: 1 ms (29.36%), Space: 20.6 MB (83.17%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for(int i = 0; i<nums.size(); i++){
-            if (count(nums.begin(), nums.end(), nums[i]) == 1) return nums[i];
-        }
-        return 0;
+        // for(int i = 0; i<nums.size(); i++){
+        //     if (count(nums.begin(), nums.end(), nums[i]) == 1) return nums[i];
+        // }
+        // return 0;
+
+        //better sol!
+        int ans=0;
+        for(int i=0;i<nums.size();i++) ans^=nums[i];
+        return ans;
     }
 };
