1685523f8d76b5cd14738fb73aa3ec8ad881a3c8
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-23 16:57:43 +0530

    Time: 0 ms (100%), Space: 11.3 MB (88.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-
-    void digitinc(vector<int>& digits, int i){
-        if (digits[i] == 9){
-            if (i == 0) {
-                digits.insert(digits.begin(), 1);
-                digits[i+1] = 0;
-                return;
-            }
-            digits[i] = 0;
-            digitinc(digits, i-1);
-        }
-        else {digits[i]++; return;}
-    }
+    // void digitinc(vector<int>& digits, int i){
+    //     if (digits[i] == 9){
+    //         if (i == 0) {
+    //             digits.insert(digits.begin(), 1);
+    //             digits[i+1] = 0;
+    //             return;
+    //         }
+    //         digits[i] = 0;
+    //         digitinc(digits, i-1);
+    //     }
+    //     else {digits[i]++; return;}
+    // }
 
     vector<int> plusOne(vector<int>& digits) {
-        int i = digits.size()-1;
-        digitinc(digits, i);
+        int i = digits.size() -1;
+        // digitinc(digits, i);
+        for(i; i>=0; i--){
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+           
+        }
+        digits.insert(digits.begin(), 1);
+
         return digits;
     }
 };
