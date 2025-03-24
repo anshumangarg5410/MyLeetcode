ef240e256eca5d9e51df0b1b44f0d71233081372
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-24 23:15:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (60.19%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.cpp
+++ 2083-three-divisors/2083-three-divisors.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int count = 2; if (n <= 2) return false;
-        for(int i = 2; i<n; i++){
-            if (n%i == 0) count++; cout << count << endl;
+        int count = 0; 
+        for(int i = 1; i<=n; i++){
+            if (n%i == 0) count++;
             if (count > 3) return false;
-        } if (count != 3) return false;
-        
-        return true;
+        } 
+        return count == 3;
     }
 };
