e7c9d25499d5b71c01f8727c537253d8473841a2
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-24 23:34:22 +0530

    Time: 0 ms (100%), Space: 24.9 MB (6.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
-        for(int i = 0; i<nums.size(); i++) {
-            if (nums[i] == 0) {
-                nums.erase(nums.begin() + i);
-                count++; i--;
-            }
-        }
-        for(int i = 0; i<count; i++) nums.push_back(0);
+        // int count = 0;
+        // for(int i = 0; i<nums.size(); i++) {
+        //     if (nums[i] == 0) {
+        //         nums.erase(nums.begin() + i);
+        //         count++; i--;
+        //     }
+        // }
+        // for(int i = 0; i<count; i++) nums.push_back(0);
+
+        //logic 2
+        vector<int>output;
+        for(int i = 0; i<nums.size(); i++) if (nums[i] != 0) output.push_back(nums[i]);
+        int a = output.size();
+        for(int i = 0; i<nums.size() - a; i++) output.push_back(0);
+        for(int i = 0; i<nums.size(); i++) nums[i] = output[i];
     }
 };
