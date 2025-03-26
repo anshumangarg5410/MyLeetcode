3d373fd5bd8805383ab1b4d5b2e0059b8cde21ad
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-26 18:51:14 +0530

    Time: 0 ms (100%), Space: 13.6 MB (42.01%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        set<int> myset(nums.begin(), nums.end());
-        myset.insert(target);
-        auto index = find(myset.begin(), myset.end(), target);
-        return distance(myset.begin(), index);
+        // set<int> myset(nums.begin(), nums.end());
+        // myset.insert(target);
+        // auto index = find(myset.begin(), myset.end(), target);
+        // return distance(myset.begin(), index);
+        //BETTER APPROACH USE BINARY SEARCH.. YET TO LEARN !! 
+
+        int start = 0; int last = nums.size() - 1;
+        while (start <= last){
+            int mid = start + (last - start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) {
+                last = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start;
+
     }
 };
