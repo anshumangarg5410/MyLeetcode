d015e4676cb0774a320752eed52e09e52807cbe4
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-28 22:39:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,5 +9,10 @@ public:
             b = c;
         }
         return b;
+
+        //using recursion 
+        // if (n == 1) return 1;
+        // if (n == 2) return 2;
+        // return climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
