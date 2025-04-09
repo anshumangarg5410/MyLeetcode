be04ee0dc4773e70c70ae8ab7a612b68803008df
Anshuman Garg <anshumangarg5410@gmail.com>
2025-04-09 22:23:09 +0530

    Time: 0 ms (100%), Space: 11.6 MB (76.32%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> zeroes;
-        vector<int> ones;
-        vector<int> twos;
+        // vector<int> zeroes;
+        // vector<int> ones;
+        // vector<int> twos;
 
-        for(int i = 0; i<nums.size();i++){
-            if (nums[i] == 0) zeroes.push_back(0);
-            else if (nums[i] == 1) ones.push_back(1);
-            else twos.push_back(2);
+        // for(int i = 0; i<nums.size();i++){
+        //     if (nums[i] == 0) zeroes.push_back(0);
+        //     else if (nums[i] == 1) ones.push_back(1);
+        //     else twos.push_back(2);
+        // }
+        // nums.clear();
+
+        // nums.insert(nums.end(), zeroes.begin(), zeroes.end());
+        // nums.insert(nums.end(), ones.begin(), ones.end());
+        // nums.insert(nums.end(), twos.begin(), twos.end());
+
+        //better way
+
+        //3 pointers approach 
+
+        int start = 0, mid = 0, end = nums.size() - 1;
+        while (mid <= end){
+            if (nums[mid] == 0) swap(nums[start++], nums[mid++]);
+            else if (nums[mid] == 2) swap(nums[end--],nums[mid]);
+            else mid++;
         }
-        nums.clear();
-
-        nums.insert(nums.end(), zeroes.begin(), zeroes.end());
-        nums.insert(nums.end(), ones.begin(), ones.end());
-        nums.insert(nums.end(), twos.begin(), twos.end());
     }
 };
