de1bfeb4477368ef557c69f2fb20ebcab5e88f97
Anshuman Garg <anshumangarg5410@gmail.com>
2025-04-27 11:31:53 +0530

    Time: 0 ms (100%), Space: 13.6 MB (97.44%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int result = 1;
+        for(int i : nums) {
+            if (i > 0) result *= 1;
+            else if (i < 0) result *= -1;
+            else result *= 0;
+        }
+        if (result > 0) return 1;
+        else if (result < 0) return -1;
+        else return 0;
+    }
+};
