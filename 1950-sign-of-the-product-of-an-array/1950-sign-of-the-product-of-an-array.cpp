bdb1795cd9caa832d084066b2efc3fa9bcad0e2c
Anshuman Garg <anshumangarg5410@gmail.com>
2025-04-27 11:33:07 +0530

    Time: 0 ms (100%), Space: 13.6 MB (79.62%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int i : nums) {
             if (i > 0) result *= 1;
             else if (i < 0) result *= -1;
-            else result *= 0;
+            else return 0;
         }
         if (result > 0) return 1;
         else if (result < 0) return -1;
