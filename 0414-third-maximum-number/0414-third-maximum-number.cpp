4d021670b44fe5eef7ef0a6534d4edc7b54b7f9b
Anshuman Garg <anshumangarg5410@gmail.com>
2025-04-29 16:47:36 +0530

    Time: 0 ms (100%), Space: 13 MB (56.86%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        auto indexx = max_element(nums.begin(), nums.end());
-        int maxeleor = *indexx;
-        if (nums.size() < 3) return *indexx;
-        int element;
-        int count = 0;
-        while (true) {
-            auto index = max_element(nums.begin(), nums.end());
-            cout << "old" << element << endl;
-            if (element != *index) count++;
-            element = *index;
-            cout << "new" << element << endl;
-            nums.erase(index);
-            // count++;
-            if (count == 2) {
-                if (element != *max_element(nums.begin(), nums.end())) return *max_element(nums.begin(), nums.end());
-            }
-            cout << nums.size()<<". " << count << endl;
-            if (nums.size() == 0) return maxeleor;
-        }
+        // auto indexx = max_element(nums.begin(), nums.end());
+        // int maxeleor = *indexx;
+        // if (nums.size() < 3) return *indexx;
+        // int element;
+        // int count = 0;
+        // while (true) {
+        //     auto index = max_element(nums.begin(), nums.end());
+        //     cout << "old" << element << endl;
+        //     if (element != *index) count++;
+        //     element = *index;
+        //     cout << "new" << element << endl;
+        //     nums.erase(index);
+        //     // count++;
+        //     if (count == 2) {
+        //         if (element != *max_element(nums.begin(), nums.end())) return *max_element(nums.begin(), nums.end());
+        //     }
+        //     cout << nums.size()<<". " << count << endl;
+        //     if (nums.size() == 0) return maxeleor;
+        // }
         
 
+        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+        for (int num : nums) {
+            if (num == first || num == second || num == third) continue;
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if (num > second) {
+                third = second;
+                second = num;
+            }
+            else if (num > third) {
+                third = num;
+            }
+        }
+        if (third == LONG_MIN) return first;
+        else return third;
     }
 };
