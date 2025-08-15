30207f66c20875ba80efadf6feb513ff0e53b579
Anshuman Garg <anshumangarg5410@gmail.com>
2025-08-15 14:09:16 +0530

    Time: 3 ms (32.64%), Space: 13.4 MB (37.41%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if (nums.size() <= 3) return nums[0];
-        for(int i = 0; i<nums.size(); i++){
-            cout << nums[i] << endl;
-            if (i > 0 && i < nums.size() - 1){
-                if (nums[i-1] != nums[i] && nums[i] != nums[i+1]) {
-                cout << nums[i] << endl;
-                return nums[i];
-                }
+        int result = 0;
+        for(int i = 0; i<32; i++) {
+            int count = 0;
+            for(int j = 0; j<nums.size(); j++) {
+                if ((nums[j] >> i) & 1 ) count++;
             }
-            else if (i == 0) {
-                if (nums[i] != nums[i+1]) {
-                cout << nums[i] << endl;
-                return nums[i];
-                }
-            }
-            else {
-                if (nums[i-1] != nums[i]) {
-                cout << nums[i] << endl;
-                return nums[i];
-                }
+            if (count %3 != 0) {
+                result = result | (1 << i);
             }
         }
-        return 0;
-        // unordered_map<int, int> freq;
-        // for(int i = 0; i<nums.size(); i++){
-        //     freq[nums[i]]++;
-        // }
-
+        return result;
     }
-
 };
-
