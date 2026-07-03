c9bb8c22b7e16937395e18b0c66e249f6f34cda9
Anshuman Garg <anshumangarg5410@gmail.com>
2026-07-03 10:43:40 +0530

    Time: 3 ms (0.99%), Space: 42.2 MB (54.2%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    long power(long x, long n) {
+
+        if (n == 0) return 1;
+        long half = power(x, n / 2);
+        long ans = (half * half) % 1000000007;
+
+        if (n % 2 == 1) ans = (ans * x) % 1000000007;
+
+        return ans;
+    }
+
+    public int countGoodNumbers(long n) {
+        if (n == 1) return 5;
+
+        long primeNo, evenNo;
+        if (n % 2 != 0) {
+            primeNo = n/2;
+            evenNo = n - primeNo;
+        }
+        else {
+            primeNo = n/2;
+            evenNo = n/2;
+        }
+        
+        long primeM = power(4, primeNo); 
+        long evenN = power(5, evenNo);
+
+        System.out.println(primeM);
+        System.out.println(evenN);
+
+        int ans = (int)((primeM * evenN) % 1000000007);
+
+        return ans;
+
+    }
+}
