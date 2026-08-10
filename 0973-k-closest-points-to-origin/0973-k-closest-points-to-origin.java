42d2df70553d6360c2e09e16335ccbd4607eab77
Anshuman Garg <anshumangarg5410@gmail.com>
2026-08-10 12:10:30 +0530

    Time: 50 ms (11.85%), Space: 52 MB (91.69%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,29 +1,29 @@
 class Solution {
-
-    public double EuclideanDist(int a, int b) {
-        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-    }
-
     public int[][] kClosest(int[][] points, int k) {
-        double[][] arr = new double[points.length][2];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((i, j) -> Double.compare(
+            ( points[j][0]*points[j][0] + points[j][1]*points[j][1]), (points[i][0]*points[i][0] + points[i][1]*points[i][1]) )
+        );
 
         for(int i = 0; i < points.length; i++) {
-            arr[i][0] = EuclideanDist(points[i][0], points[i][1]);
-            arr[i][1] = i;
+            pq.add(i);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+
         }
 
-        Arrays.sort(arr, (a, b) -> Double.compare(a[0], b[0]));
-
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i][0] + " at " + arr[i][1]);
-        }
-
-        int[][] ans = new int[k][1];
+        int[][] ans = new int[k][2];
 
         for(int i = 0; i < k; i++) {
-            ans[i] = points[(int)arr[i][1]];
+            int index = pq.poll();
+
+            ans[i][0] = points[index][0];
+            ans[i][1] = points[index][1];
         }
 
         return ans;
+
+
     }
 }
